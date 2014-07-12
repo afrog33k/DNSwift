b6b2b34718e4a89a9b75c919876e526b2bb3a0e3
Matthew Sotoudeh <matthewsot@outlook.com>
2014-07-11 17:08:41 -0700

    changing it to a different project layout
    to make up for the lack of namespaces

---
+++ System/System.Collections/System.Collections.Generic/System.Collections.Generic/IList.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+/*protocol IList: IEnumerable, ICollection {
+    typealias T;
+    var Count: Int { get };
+    
+    func IndexOf(obj: AnyObject) -> Int;
+    func Add(obj: T);
+    func AddRange(objs: T[]);
+    func RemoveAt(index: Int);
+    func Remove(obj: AnyObject);
+    func RemoveRange(objs: AnyObject[]);
+    func RemoveAll();
+    func RemoveAll(predicate: (T) -> Bool);
+    func Insert(index: Int, obj: T);
+    func InsertRange(startingIndex: Int, objs: T[]);
+    
+    func generate() -> Enumerator<T>;
+}*/
---
+++ System/System.Collections/System.Collections/IEnumerator.swift
@@ -0,0 +1,18 @@
+//
+//  IEnumerator.swift
+//  DNSwift
+//
+//  Created by Matthew S on 6/9/14.
+//  Copyright (c) 2014 Matthew S. All rights reserved.
+//
+
+import Foundation
+
+//http://msdn.microsoft.com/en-us/library/78dfe2yb%28v=vs.110%29.aspx
+/*protocol IEnumerator: Generator {
+    typealias T;
+    //var Current: T? { get }
+    func MoveNext() -> Bool;
+    func Reset();
+    func next() -> T?;
+}*/
