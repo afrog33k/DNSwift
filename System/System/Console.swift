5fee13c8f2a3a0d4aa5ba6571e3a42f0e5ec6a4a
Matthew Sotoudeh <matthewsot@outlook.com>
2014-07-21 20:32:47 -0700

    Added System project

---
+++ System/System/Console.swift
@@ -0,0 +1,9 @@
+//
+//  Console.swift
+//  System
+//
+//  Created by Matthew S on 7/21/14.
+//  Copyright (c) 2014 Matthew S. All rights reserved.
+//
+
+import Foundation
---
+++ System/System/System.h
@@ -0,0 +1,19 @@
+//
+//  System.h
+//  System
+//
+//  Created by Matthew S on 7/21/14.
+//  Copyright (c) 2014 Matthew S. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for System.
+FOUNDATION_EXPORT double SystemVersionNumber;
+
+//! Project version string for System.
+FOUNDATION_EXPORT const unsigned char SystemVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <System/PublicHeader.h>
+
+
