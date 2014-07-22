53356decfc04f3ccf4971047517fd6dee97aeea3
Matthew Sotoudeh <matthewsot@outlook.com>
2014-07-21 20:34:02 -0700

    added Console class

--- System/System/Console.swift
+++ System/System/Console.swift
@@ -1,9 +1,25 @@
-//
-//  Console.swift
-//  System
-//
-//  Created by Matthew S on 7/21/14.
-//  Copyright (c) 2014 Matthew S. All rights reserved.
-//
-
 import Foundation
+
+class Console {
+    class func Write(str: String) {
+        print(str);
+    }
+    
+    class func WriteLine(line: String) {
+        println(line);
+    }
+    
+    class func Read() -> String? {
+        //TODO
+        return nil;
+    }
+    
+    class func ReadLine() -> String {
+        //http://stackoverflow.com/questions/24004776/input-from-the-keyboard-in-command-line-application
+        var keyboard = NSFileHandle.fileHandleWithStandardInput()
+        var inputData = keyboard.availableData
+        return NSString(data: inputData, encoding:NSUTF8StringEncoding).stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet());
+    }
+    
+    //TODO: Swift doesn't seem to have great support for a lot of console stuff, add more later
+}
