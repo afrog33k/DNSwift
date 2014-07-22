066c9a02e1e4ae84e0c4bd67970c205038577341
Matthew Sotoudeh <matthewsot@outlook.com>
2014-07-21 20:17:22 -0700

    Removed all the old code
    I’ll replace it with a new layout

--- DNSwift/DNSwift/System/Collections/Generic/IEnumerable.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-//http://msdn.microsoft.com/en-us/library/9eekhta0%28v=vs.110%29.aspx
-protocol IEnumerable: Sequence {
-    //typealias T;
-    
-    func GetEnumerator<IE where IE:IEnumerator>() -> IE;
-}
