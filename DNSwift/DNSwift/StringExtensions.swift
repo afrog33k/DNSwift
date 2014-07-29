3620180652c64ee4872db1a2d600b938464a6b1d
Matthew Sotoudeh <matthewsot@outlook.com>
2014-07-28 19:11:08 -0700

    StringExtensions marked public
    issue #1

--- DNSwift/DNSwift/StringExtensions.swift
+++ DNSwift/DNSwift/StringExtensions.swift
@@ -8,49 +8,49 @@
 
 import Foundation
 
-extension String {
+public extension String {
     //Allows for str[i] and str[i..<m]
     //Thanks! http://stackoverflow.com/questions/24092884/get-nth-character-of-a-string-in-swift-programming-language
-    subscript (i: Int) -> String {
+    public subscript (i: Int) -> String {
         return String(Array(self)[i]);
     }
     
-    subscript (r: Range<Int>) -> String {
+    public subscript (r: Range<Int>) -> String {
         var start = advance(startIndex, r.startIndex);
         var end = advance(startIndex, r.endIndex);
         return substringWithRange(Range(start: start, end: end));
     }
     
-    func ToString() -> String {
+    public func ToString() -> String {
         return self;
     }
     
-    func Split(splitAt: String) -> [String] {
+    public func Split(splitAt: String) -> [String] {
         return self.componentsSeparatedByString(splitAt);
     }
     
-    func ToLower() -> String {
+    public func ToLower() -> String {
         return self.lowercaseString;
     }
     
-    func ToUpper() -> String {
+    public func ToUpper() -> String {
         return self.uppercaseString;
     }
     
-    func Length() -> Int {
+    public func Length() -> Int {
         return countElements(self);
     }
     
-    static func Empty() -> String {
+    public static func Empty() -> String {
         return String();
     }
     
-    func Trim() -> String {
+    public func Trim() -> String {
         return self.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet());
     }
     
     //TrimEnd() and TrimStart() have pretty skech implementations - should rewrite them
-    func TrimEnd() -> String {
+    public func TrimEnd() -> String {
         var numToTrim = 0;
         for(var i = (self.Length() - 1); i >= 0; i--) {
             var curr = self[i];
@@ -66,7 +66,7 @@ extension String {
         return self[0..<(self.Length() - numToTrim)];
     }
     
-    func TrimStart() -> String {
+    public func TrimStart() -> String {
         var numToTrim = 0;
         for(var i = 0; i < self.Length(); i++) {
             var curr = self[i];
@@ -82,7 +82,7 @@ extension String {
         return self[numToTrim...(self.Length() - 1)];
     }
     
-    func IndexOf(substr: String) -> Int {
+    public func IndexOf(substr: String) -> Int {
         var rangeOfStr = self.rangeOfString(substr);
         if(rangeOfStr == nil) {
             return -1;
@@ -91,15 +91,15 @@ extension String {
         //Thanks! Sulthan @ stackoverflow 24029163
     }
     
-    func Contains(substr: String) -> Bool {
+    public func Contains(substr: String) -> Bool {
         return self.IndexOf(substr) != -1;
     }
     
-    func StartsWith(prefix: String) -> Bool {
+    public func StartsWith(prefix: String) -> Bool {
         return self.hasPrefix(prefix);
     }
     
-    func EndsWith(suffix: String) -> Bool {
+    public func EndsWith(suffix: String) -> Bool {
         return self.hasSuffix(suffix);
     }
 }
